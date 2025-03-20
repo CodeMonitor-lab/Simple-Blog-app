44ebe71f793dfff7f731d8fd439413bec3861381
CodeMonitor-lab <niteshcse3333@gmail.com>
2025-03-20 23:32:10 +0530

    Update Files

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Styles from '../Styles/Home.module.css'
-import { Hero_Section } from '../Components/index'
+import { Hero_Section,Login,SignUp } from '../Components/index'
 
 const Home = () => {
     return (
         <div>
             <div className={Styles.Home_Container}>
                 <Hero_Section />
-                <h1 className={Styles.Home_Section} >Home page</h1>
+                <h1 style={{color:'blueviolet',textAlign:'center',fontFamily:'sans-serif',textDecoration:'underline 0.2rem'}} >Home Page</h1>
+                <Login/>
+                
             </div>
         </div>
     )
