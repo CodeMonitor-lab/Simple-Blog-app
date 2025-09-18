8829615ff56bc63a7b84158dd385776e79caa656
CodeMonitor-lab <niteshcse3333@gmail.com>
2025-09-18 16:50:55 +0300

    Adding Yarn WorkSpace

---
+++ web/next.config.mjs
@@ -0,0 +1,13 @@
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  turbopack: {
+    root: path.resolve(__dirname, ".."), // 👈 go up to Simple-Blog-app root
+  },
+};
+
+export default nextConfig;
